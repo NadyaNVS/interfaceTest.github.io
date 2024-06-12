fd479684f11911e9189f32d92c3eb4697fd3cd09
NadyaNVS <105170990+NadyaNVS@users.noreply.github.com>
2024-06-12 20:56:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
+console.log(tg.initDataUnsafe);
 
 const userName = document.getElementById("user-info");
 const avatarImg = document.getElementById("user-avatar");
