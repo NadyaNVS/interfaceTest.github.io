abda2b4fb007315ee959e062754d25c5c0d83dde
NadyaNVS <105170990+NadyaNVS@users.noreply.github.com>
2024-06-13 19:08:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
+document.addEventListener("DOMContentLoaded", function () {
+  let tg = window.Telegram.WebApp;
+  tg.expand();
 
-const userName = document.getElementById("user-info");
-userName.innerHTML = `${tg.initDataUnsafe.user.username}`;
+  // Simulate fetching
+  setTimeout(() => {
+    if (tg.initDataUnsafe.user) {
+      const user = tg.initDataUnsafe.user;
+      const userName = document.getElementById("user-info");
+      userName.innerHTML = `${user.username}`;
 
 //only in attachment menu
 // const avatarImg = document.getElementById("user-avatar");
 // avatarImg.src = tg.initDataUnsafe.user.photo_url;
+    } else {
+      const userName = document.getElementById("user-info");
+      userName.innerHTML = `Hi, there!`;
+      console.error("User data is not available");
+    }
+
+    document.getElementById("content").classList.remove("hidden");
+    document.getElementById("loader").style.display = "none";
+  }, 3000);
+});
+
